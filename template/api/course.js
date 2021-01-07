27d0e0d054afb7c40fd23171de4ddb8413dc4cf2
Danbro007 <710170342@qq.com>
2021-01-07 13:43:39 +0800

    添加课程分页以及带有条件的查询

--- template/api/course.js
+++ template/api/course.js
@@ -1,11 +1,36 @@
 import request from '@/utils/request'
 
 export default {
-    //查询热门课程
+  //查询热门课程
   getTopCourseList(num) {
     return request({
       url: `/edu/front/course/top/${num}`,
       method: 'get'
     })
+  },
+  //条件分页课程查询的方法
+  getCourseList(page, limit, searchObj) {
+    return request({
+      url: `/edu/front/course/${page}/${limit}`,
+      method: 'post',
+      data: searchObj
+    })
+  },
+  //查询所有分类的方法
+  getAllSubject() {
+    return request({
+      url: '/edu/subject',
+      method: 'get'
+    })
+  },
+  //课程详情的方法
+  getCourseInfo(id) {
+    return request({
+      url: '/eduservice/coursefront/getFrontCourseInfo/' + id,
+      method: 'get'
+    })
   }
+
 }
+
+
